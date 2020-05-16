40d208f1b78f87ab08925cd938bb5d4e5cb82645
davieu <davisumana@gmail.com>
2020-05-16 17:57:35 -0400

    redo

--- server/index.js
+++ server/index.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oath20').Strategy;
-
 const app = express();
 
-// makes passport aware that a new strategy is available. Creating a new instance of the GoogleStrat constructor
-passport.use(new GoogleStrategy());
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 
-/*
-CLIENT ID
-904004713758-nbgert4no10khi5vhgiosjeba09tqo5n.apps.googleusercontent.com
 
-CLIENT SECRET
-WawVipnLlqxvcmSBalRzzZGg
-*/
 
 
