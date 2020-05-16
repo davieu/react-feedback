06ae203c0b7088b0b5212b5cebdd11b358135e3a
davieu <davisumana@gmail.com>
2020-05-16 19:11:45 -0400

    route handler for google oauth

--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
 const express = require('express');
-const GoogleStrategy = require('passport-google-oath20').Strategy;
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+/*
+makes passport aware that a new strategy is available
+Creating a new instance of the GoogleStrat constructor
+we put in an object with the keys as the first argument and a callback url.
+2nd argument is the accesstoken function
+*/
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
-// makes passport aware that a new strategy is available
-// Creating a new new instance of the GoogleStrat constructor
-passport.use(new GoogleStrategy());
+/* 
+route handler for when user clicks google oauth button.
+1st arg is the route
+2nd arg is passport.auth which tells passport what strategy to use ('google') and
+also takes in an object with scope. The scope is what we want google to send us. in
+this case the profile and email info. scope is part of google and their are many
+scope options that they have.
+*/
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
-
-
-
