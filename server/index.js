00785e5257279dcc685966e7a740db685bb08e35
davieu <davisumana@gmail.com>
2020-05-16 18:06:22 -0400

    made keys config file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const GoogleStrategy = require('passport-google-oath20').Strategy;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({hi: 'there'});
 });
 
-
+// makes passport aware that a new strategy is available
+// Creating a new new instance of the GoogleStrat constructor
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
