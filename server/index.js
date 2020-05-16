0c1a1258e54bf13e647acbe4169d6344555faadb
davieu <davisumana@gmail.com>
2020-05-16 17:41:20 -0400

    created google oauth credentials and added passport

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oath20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
-
-
+// makes passport aware that a new strategy is available. Creating a new instance of the GoogleStrat constructor
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 
+/*
+CLIENT ID
+904004713758-nbgert4no10khi5vhgiosjeba09tqo5n.apps.googleusercontent.com
 
+CLIENT SECRET
+WawVipnLlqxvcmSBalRzzZGg
+*/
 
 
