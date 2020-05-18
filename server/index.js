083fa49aa40a1e182d138c1073086179b2b4a13c
davieu <davisumana@gmail.com>
2020-05-17 22:42:23 -0400

    made mongoatlas cluster for app and added key to keyjs file also connected the app to mongodb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 // const or a variable not needed since we are simply just importing file
 require('./services/passport');
 const app = express();
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => console.log('Error on start:' + err.stack));
 
-// function from the routes/authRoutes file. app is used as the arg
+// function from the routes/authRoutes file. app object is used as the argument
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
