eed31b315fdb38d45079c72257c4dd60ba965079
davieu <davisumana@gmail.com>
2020-05-17 23:56:34 -0400

    implemented so duplicate user doesnt get added in passport file

--- server/services/passport.js
+++ server/services/passport.js
@@ -21,7 +21,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (!existingUser) {
+          console.log('ADDEDEDED');
+          new User({ googleId: profile.id }).save();
+        } else {
+          console.log('User with ID already in DB: ' + existingUser);
+        }
+      });
+
+      // new User({ googleId: profile.id }).save();
     }
   )
 );
