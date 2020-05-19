56c0a48d1d03fac33a09b025853a2231c71959c6
davieu <davisumana@gmail.com>
2020-05-18 22:25:16 -0400

    redo

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,13 +1,9 @@
 // keys.js - figure out what set of credentials to return
-// we are in production -- return the prod set of keys
-// module.exports = require('./prod');
+
 if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY,
-  };
+  // we are in production -- return the prod set of keys
+  module.exports = require('./prod');
 } else {
+  // we are in development - return the dev keys
   module.exports = require('./dev');
 }
