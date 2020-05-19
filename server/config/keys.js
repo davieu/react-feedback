f0da1c7b620ee71c9344f6e78f5254329c3924f6
davieu <davisumana@gmail.com>
2020-05-18 21:09:49 -0400

    t6

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 // keys.js - figure out what set of credentials to return
-
+/*
 if (process.env.NODE_ENV === 'production') {
   // we are in production -- return the prod set of keys
   module.exports = require('./prod');
@@ -7,3 +7,10 @@ if (process.env.NODE_ENV === 'production') {
   // we are in development - return the dev keys
   module.exports = require('./dev');
 }
+*/
+const { NODE_ENV: env } = process.env;
+
+const config =
+  env === 'production' ? require('./prod').default : require('./dev').default;
+
+export default config;
