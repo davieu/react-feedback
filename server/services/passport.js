2352beeb55fc0fdaa06a831313cd99379611e112
davieu <davisumana@gmail.com>
2020-05-18 21:32:07 -0400

    heroku

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const keys = require('../config/keys');
 
 // pull a model out of mongoose to use
